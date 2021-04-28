b72e0486ff807a400e6011ef85be99c7587f9b69
ArtemZ154 <Artemz154@ya.ru>
2021-04-28 17:13:51 +0700

    new

--- Files/JS/playstation5_page_js.js
+++ Files/JS/playstation5_page_js.js
@@ -17,7 +17,7 @@ if (animItems.length > 0) {
             if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
                 animItem.classList.add('_active');
             } else {
-                if (animItem.classList.contains('_anim_no_restart')) {
+                if (!animItem.classList.contains('_anim_no_restart')) {
                     animItem.classList.remove('_active');
                 }
                 
