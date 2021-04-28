cd83ff0e0741106048c9255b74cc20e17348db5b
ArtemZ154 <Artemz154@ya.ru>
2021-04-28 10:54:54 +0700

    new

---
+++ Files/JS/playstation5_page_js.js
@@ -0,0 +1,37 @@
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', anim_on_scroll)
+    function anim_on_scroll(params) {
+        
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = (window.innerHeight - animItemHeight / animStart) - 200;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = (window.innerHeight - window.innerHeight / animStart) - 200;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                if (animItem.classList.contains('_anim_no_restart')) {
+                    animItem.classList.remove('_active');
+                }
+                
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scroolLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scroolLeft }
+    }
+
+    setTimeout(() => {
+        anim_on_scroll();
+    }, 100)
